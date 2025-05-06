e18fb79406e7bc84aa932fddd4c02646abead462
jorgePrograma <jemartelo07@gmail.com>
2025-05-06 17:02:39 -0500

    libreria de workspace2

---
+++ shared-lib-workspace/projects/shared-state/src/lib/shared-state.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-shared-state',
+  imports: [],
+  template: `
+    <p>
+      shared-state works!
+    </p>
+  `,
+  styles: ``
+})
+export class SharedStateComponent {
+
+}
---
+++ shared-lib-workspace/projects/shared-state/src/public-api.ts
@@ -0,0 +1,6 @@
+/*
+ * Public API Surface of shared-state
+ */
+
+export * from './lib/shared-state.service';
+export * from './lib/shared-state.component';
